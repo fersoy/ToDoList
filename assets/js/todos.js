f0bf8b14ae44feba8c0e4fba5dc6908d54c48a60
Fatih Ersoy <f.ersoytx@gmail.com>
2019-09-29 14:53:19 -0500

    the project is done

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,19 +1,22 @@
-$("ul").on("click", "li", function(){
- $(this).toggleClass("completed");
-}); 
+$("ul").on("click", "li", function () {
+    $(this).toggleClass("completed");
+});
 
-$("ul").on("click", "span", function(e){
-    $(this).parent().fadeOut(500, function(){
+$("ul").on("click", "span", function (e) {
+    $(this).parent().fadeOut(500, function () {
         $(this).remove();
     });
     e.stopPropagation();
 });
 
-$("input[type='text']").keypress(function(e){
-    if(e.which === 13){
+$("input[type='text']").keypress(function (e) {
+    if (e.which === 13) {
         var todoText = $(this).val();
         $(this).val("");
 
-        $("ul").append("<li><span>X</span> " + todoText + "</li>")
+        $("ul").append("<li><span><i class='fas fa-trash-alt'></i></span> " + todoText + "</li>")
     }
 });
+$(".fa-plus").click(function () {
+    $("input[type='text']").fadeToggle();
+})
